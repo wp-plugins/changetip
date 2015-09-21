a6022d11c28603a2ec8fdc13dd21cc773fa427f5
evannagle <evannagle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-21 23:09:30 +0000

    0.0.8 update
    git-svn-id: https://plugins.svn.wordpress.org/changetip/tags/0.0.8@1250755 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/changetip-client.js
@@ -0,0 +1,47 @@
+
+'use strict';
+
+(function($, changetip) {
+    $(function() {
+        $.getScript(changetip.origin + "/public/js/widgets.js")
+        .done(function() {
+
+            // timeout to let tip.me button load, then bring the default zindex down a notch so that
+            // it doesn't cover standard wordpress nav bars.
+            window.setTimeout(function() {
+                $(".changetip_tipme_button").each(function(idx, button) {
+                    var popoverIframe = $(button).find("iframe").first();
+                    if(popoverIframe.length>0) {
+                        popoverIframe[0].style.zIndex = 99999;
+                    }
+                });
+            }, 1000)
+
+            if(changetip.options.approve) {
+                var contextUrl = changetip.options.approve.context_url;
+                changetip.popover('approve', {
+                    comment: changetip.options.approve.comment_content,
+                    contextUrl: contextUrl, // reference the url to the blog entry's comment
+                    commentId: changetip.uuid.UUID.v5(contextUrl) // unique but always the same to prevent duplicate tips
+                });
+            }
+
+            changetip.listen('message', function(e) {
+                if(e.origin == changetip.origin && e.data ) {
+                    var data = e.data;
+                    if(data.success) {
+                        if(changetip.options.approve) {
+                            changetip.autoreplyToComment(data.meta, changetip.options.approve);
+                        } else {
+                            changetip.autoreplyToTip(data.meta);
+                        }
+                    } else if(data.close) {
+                        changetip.closePopover();
+                    }
+                }
+            });
+        })
+        .fail(function() {
+        });
+    });
+})(jQuery, window.changetip);
